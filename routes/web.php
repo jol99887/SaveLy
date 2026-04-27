df7e0d0535fbfc6636df573115f460970860f9bf
jol99887 <154651550+jol99887@users.noreply.github.com>
2026-04-27 17:41:45 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExpenseController;
 
 Route::get('/', function () {
     return view('dashboard.index');
 });
+
+Route::get('/expense', [ExpenseController::class, 'create']);
+Route::post('/expense/store', [ExpenseController::class, 'store']);
+
